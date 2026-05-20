6cf493aeb6fb6dee19ebf72d1ba2ed0d8a37c265
Rajommkar <marthaommkarnath@gmail.com>
2026-05-20 13:39:01 +0530

    feat(api): implement AuthModule with JwtModule configuration

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+
+import { JwtModule } from '@nestjs/jwt';
+
 import { UsersModule } from '../users/users.module';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'super-secret',
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
+
   controllers: [AuthController],
+
   providers: [AuthService],
 })
 export class AuthModule {}
