9873248515828eb4b311d9079a1263bea57a3bee
Rajommkar <marthaommkarnath@gmail.com>
2026-05-20 12:59:39 +0530

    feat(api): implement UsersService with query and create operations

--- apps/api/src/modules/users/users.service.ts
+++ apps/api/src/modules/users/users.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
+
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async createUser(data: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+  }) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
 }
